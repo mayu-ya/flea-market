830b832f1e8767539ec14b7c0f5a8f476976c06b
mayu <8barosl9@gmail.com>
2026-04-08 00:03:13 +0900

    2026/04/07 commit

---
+++ .history/src/resources/views/purchase.blade_20260407115452.php
@@ -0,0 +1,101 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/purchase.css') }}">
+@endsection
+
+@section('content')
+<form class="form" action="{{ route('purchase.store', [$merchandise->id]) }}" method="post">
+    @csrf
+    <div class="content">
+        <div class="content-item">
+            <div class="item">
+                <img src="{{ asset($merchandise->image) }}" alt="商品画像" class="item-img">
+            </div>
+            <div class="item">
+                <div class="item-input">
+                    <input type="hidden" name="merchandise_id" value="{{ $merchandise->id }}">
+                    <input type="text" class="input" value="{{ $merchandise->merchandise_name }}" readonly><br>
+                    <label>￥<input type="text" class="input" value="{{ $merchandise->price }}" readonly></label>
+                </div>
+            </div>
+        </div>
+        <table class="table">
+            <tr class="table-tr">
+                <th class="table-th">商品代金</th>
+                <td class="table-td">{{ $merchandise->price }}</td>
+            </tr>
+            <tr class="table-tr">
+                <th class="table-th">支払方法</th>
+                <td class="table-td">
+                    <div id="ajax_result"></div>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="content">
+        <div class="pay">
+            <div class="pay-item">支払い方法</div>
+            <select name="pay" id="ajax_select" class="pay-select">
+                <option value="">選択してください</option>
+                <option value="1">コンビニ払い</option>
+                <option value="2">カード支払い</option>
+            </select>
+        </div>
+        <div class="button">
+            <button class="button-submit" type="submit">購入する</button>
+        </div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+        @endif
+    </div>
+    <div class="content">
+        <div class="address">
+            <div class="address-content">
+                <div class="address-item">配送先</div>
+                <div class="address-a">
+                    <a class="address-a-nav" href="{{ route('address.index', [$merchandise->id]) }}">変更する</a>
+                </div>
+            </div>  
+            <div class="address-in">
+                <label>〒<input type="text" name="post_code" value="{{ $profile->post_code }}" readonly></label><br>
+                <input type="text" class="address-input" name="address" value="{{ $profile->address }}" readonly>
+                <input type="text" class="address-input" name="building" value="{{ $profile->building ?? '' }}" readonly>
+                <input type="hidden" name="profile_id" value="{{ $profile->id }}">
+            </div>
+        </div>
+    </div>
+</form>
+@endsection
+
+@section('js')
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+<script>
+    $(function(){
+        $('#ajax_select').on('change', function(){
+            var input_value = $(this).val();
+
+            if(input_value === "") {
+                $('#ajax_result').html("");
+                return;
+            }
+            $.ajax({
+                headers:{ 'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')},
+                url:"{{ route('purchase.ajax') }}",
+                type: 'POST',
+                data:{"ajax_select":input_value},
+            })
+            .done(function(data){
+                $('#ajax_result').html(data['form']);
+            })
+            .fail(function(){
+                alert('error');
+            })
+        });
+    });
+</script>
+@endsection
---
+++ .history/src/resources/views/sell.blade_20260407153843.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/sell.css') }}">
+@endsection
+
+@section('content')
+<form class="form" action="/" method="post" enctype="multipart/form-data">
+    @csrf
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+        @endif
+    <div class="title">
+        <h1 class="title-logo">商品の出品</h1>
+    </div>
+    <div class="content">
+        <h3 class="content-h">商品画像</h3>
+        <input type="file" class="content-fifle" name="image">
+        <input type="hidden" name="profile_id" value="{{ old('profile_id', $profile_id ?? '' ) }}">
+    </div>
+
+    <div class="detail">
+        <h2 class="title-h">商品の詳細</h2>
+    </div>
+    <div class="content">
+        <h3 class="content-h">カテゴリー</h3>
+        @foreach($categories as $category)
+        <label><input type="checkbox" name="content[]" value="{{ old('content[]', $category['id'] ?? '' ) }}"><span>{{ $category['content'] }}</span></label>
+        @endforeach
+    </div>
+    <div class="content">
+        <h3 class="content-h">商品の状態</h3>
+        <select class="content-condition" name="condition" value="{{ old('condition') }}">
+            <option value="">選択してください</option>
+            <option value="1">良好</option>
+            <option value="2">目立った外傷なし</option>
+            <option value="3">やや傷や汚れあり</option>
+            <option value="4">状態が悪い</option>
+        </select>
+    </div>
+
+    <div class="detail">
+        <h2 class="title-h">商品名と説明</h2>
+    </div>
+    <div class="content">
+        <h3 class="content-h">商品名</h3>
+        <input type="text" class="content-text" name="merchandise_name" value="{{ old('merchandise_name') }}">
+    </div>
+    <div class="content">
+        <h3 class="content-h">ブランド名</h3>
+        <input type="text" class="content-text" name="brand_name" value="{{ old('brand_name') }}">
+    </div>
+    <div class="content">
+        <h3 class="content-h">商品の説明</h3>
+        <textarea class="content-textarea" name="explanation"> {{ old('explanation') }}</textarea>
+    </div>
+    <div class="content">
+        <h3 class="content-h">販売価格</h3>
+        <label for="price">￥</label>
+        <input type="text" class="content-text" name="price" id="price"  value="{{ old('price') }}">
+    </div>
+
+    <div class="button">
+        <button class="button-submit">出品する</button>
+    </div>
+</form>
+@endsection
